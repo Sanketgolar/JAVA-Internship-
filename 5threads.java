f2ef0caa5e3ad241109ae852318a14b08b08bf2a
Sanketgolar <110994081+Sanketgolar@users.noreply.github.com>
2022-08-10 20:53:47 +0530

    Update 5threads.java

--- 5threads.java
+++ 5threads.java
@@ -1,1 +1,27 @@
+class Mythread extends Thread{
 
+    @Override
+    public void run() {
+        System.out.println(getName()+"  Hello All!");
+    }
+}
+
+
+class Main{
+
+    public static void main(String[] args){
+
+        Thread t1=new Mythread();
+        Thread t2=new Mythread();
+        Thread t3=new Mythread();
+        Thread t4=new Mythread();
+        Thread t5=new Mythread();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+    }
+
+}
