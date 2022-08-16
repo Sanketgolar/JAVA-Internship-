f4bb1d86bcb116e228f71bf7d71eae01e8265aa9
Sanketgolar <110994081+Sanketgolar@users.noreply.github.com>
2022-08-16 08:35:31 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main
+{
+
+
+    public static void main(String[] args)
+    {
+        List<Integer> hs=new ArrayList<>();
+        hs.add(45);
+        hs.add(56);
+        hs.add(4);
+        hs.add(95);
+        hs.add(0);
+        hs.add(9);
+        hs.add(1);
+        System.out.println(hs.toString());
+        //ordered insertion at list
+    }
+}
